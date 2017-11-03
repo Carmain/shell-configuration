df7fe0c7a9423d8077a2eba8fe124e15ca7b1ec7
Martin Billard <martinez@iMac-de-Martin.local>
2017-11-03 14:11:20 +0100

    Improve the installer

--- configuration.sh
+++ configuration.sh
@@ -2,22 +2,28 @@
 
 clear
 
-# install vim
-sudo apt-get install vim
+echo " - Moving .vimrc file"
+cp .vimrc ~/.vimrc
 
-# install pathogen
+echo " - Moving .gitconfig file"
+cp .gitconfig ~/.gitconfig
+
+echo " - Installing Pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+echo " - Installing vim modules"
+
 # go to directory
-cd ~/.vim/bundle
+cd ~/.vim/bundle 
 
 # install modules
 git clone https://github.com/scrooloose/nerdtree # The Nerd Tree
 git clone https://github.com/kien/ctrlp.vim      # CTRLP.VIM
 
 # install themes
-git clone https://github.com/altercation/vim-colors-solariz # Solarized
+git clone https://github.com/altercation/vim-colors-solarized # Solarized
 git clone https://github.com/w0ng/vim-hybrid                # Hybrid
 git clone https://github.com/jonathanfilip/vim-lucius       # Lucius
 git clone https://github.com/sickill/vim-monokai            # Monokai
+
