7352deeb2fbbb61577b901898d5ecd4a65a50c8c
Carmain <martin.l.billard@gmail.com>
2017-11-04 16:00:39 +0100

    Improve the install script

--- configuration.sh
+++ configuration.sh
@@ -2,28 +2,32 @@
 
 clear
 
-echo " - Moving .vimrc file"
+function display {
+    printf "\033[34m\033[1m - $1 \033[0m \n"
+}
+
+display "Moving .vimrc"
 cp .vimrc ~/.vimrc
 
-echo " - Moving .gitconfig file"
+display "Moving .gitconfig"
 cp .gitconfig ~/.gitconfig
 
-echo " - Installing Pathogen"
+display "Moving .zshrc"
+cp .zshrc ~/.zshrc
+
+display "Installing Pathogen"
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-echo " - Installing vim modules"
-
-# go to directory
-cd ~/.vim/bundle 
+display "Installing vim modules"
+cd ~/.vim/bundle  # go to directory
 
 # install modules
 git clone https://github.com/scrooloose/nerdtree # The Nerd Tree
 git clone https://github.com/kien/ctrlp.vim      # CTRLP.VIM
 
 # install themes
-git clone https://github.com/altercation/vim-colors-solarized # Solarized
-git clone https://github.com/w0ng/vim-hybrid                # Hybrid
-git clone https://github.com/jonathanfilip/vim-lucius       # Lucius
-git clone https://github.com/sickill/vim-monokai            # Monokai
-
+git clone https://github.com/altercation/vim-colors-solarized  # Solarized
+git clone https://github.com/w0ng/vim-hybrid                   # Hybrid
+git clone https://github.com/jonathanfilip/vim-lucius          # Lucius
+git clone https://github.com/sickill/vim-monokai               # Monokai
